da140424960fd7da46c4d9ef98113283b1e2c3f1
Ryan Fink <rfink@redventures.net>
2012-05-11 19:17:42 +0000

    Use zend conventions for methods

--- lib/Context/ContextInterface.php
+++ lib/Context/ContextInterface.php
@@ -13,7 +13,7 @@ interface ContextInterface
      * @param string $key
      * @return string
      */
-    public function get_value($key);
+    public function getValue($key);
     
     /**
      * Set value with the given key/val
@@ -21,5 +21,5 @@ interface ContextInterface
      * @param mixed $value
      * @return ContextInterface
      */
-    public function set_value($key, $value);
+    public function setValue($key, $value);
 }
